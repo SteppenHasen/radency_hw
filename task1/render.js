acca18633464ec97073c73df3885f3f40846ac2e
SteppenHasen <dustasura@gmail.com>
2022-01-08 21:20:38 +0200

    Init task1

---
+++ task1/render.js
@@ -0,0 +1,12 @@
+function createNoteDOM(noteID) {
+    let notes_area = document.getElementById('notes_area')
+    let note = document.createElement('tr')
+    
+    let icon = document.createElement('td')
+    let name = document.createElement('td')
+    let date = document.createElement('td')
+    let category = document.createElement('td')
+    let content = document.createElement('td')
+    let dates = document.createElement('td')
+    let buttons = document.createElement('td')
+}
