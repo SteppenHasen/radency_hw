5fbd3c836018044b237fbcde9b3ca57062eda0cc
SteppenHasen <dustasura@gmail.com>
2022-01-09 18:54:50 +0200

    end with stats render & refactored notes render

--- task1/render.js
+++ task1/render.js
@@ -6,30 +6,39 @@ let categories = {
     animals:'url(democrat-solid.svg)'
 }
 
-function createNoteDOM(noteID, noteData) {
-    let notes_area = document.getElementById('notes_area')
+function createNoteDOM(noteID, noteData, dom) {
     let note = document.createElement('tr')
-    
-    let icon = document.createElement('td')
-    let name = document.createElement('td')
-    let date = document.createElement('td')
-    let category = document.createElement('td')
-    let content = document.createElement('td')
-    let dates = document.createElement('td')
-    let buttons = document.createElement('td')
+    dom.appendChild(note)
 
-    icon.style.backgroundImage = categories[noteData.category]
-    name.innerHTML = `<p>${noteData.name}</p>`
-    date.innerHTML = `<p>${noteData.date}</p>`
-    category.innerHTML = `<p>${noteData.category}</p>`
-    content.innerHTML = `<p>${noteData.content}</p>`
-    dates.innerHTML = `<p>${noteData.dates}</p>`
+    if(noteData.archived == true) {
+        note.style.display = 'none'
+    }
 
-    [icon, name, date, category, content, dates, buttons].forEach(node => note.appendChild(node))
+    createTdNodeForNotes('icon', note, noteData)
+    createTdNodeForNotes('name', note, noteData)
+    createTdNodeForNotes('date', note, noteData)
+    createTdNodeForNotes('category', note, noteData)
+    createTdNodeForNotes('content', note, noteData)
+    createTdNodeForNotes('dates', note, noteData)
+    createTdNodeForNotes('buttons', note, noteData)
 
     createButton(buttons, noteID, 'edit')
     createButton(buttons, noteID, 'archive')
     createButton(buttons, noteID, 'delete')
+}
+
+function createTdNodeForNotes(name, node_to_append, data) {
+    let node = document.createElement('td')
+    node.className = name
+    
+    if(name == 'icon') {
+        icon.style.backgroundImage = categories[data.category]
+    } else {
+        node.innerHTML = `<p>${data[name]}</p>`
+    }
+
+    node_to_append.appendChild(node)
+
 }
 
 function createButton(node, id, type) {
@@ -47,4 +56,38 @@ function createButton(node, id, type) {
     node.appendChild(button)
 }
 
-export {createNoteDOM}
+function createTdNodeForStats(node, data) {
+    let td = document.createElement('td')
+        td.innerHTML = data
+        node.appendChild(td)
+}
+
+function createStatDOM(categories, notes, dom) {
+
+    Object.keys(categories).forEach(cat => {
+        let categoryRow = document.createElement('tr')
+
+        let notesForCategory = notes.filter(elem => {return elem.category == cat })
+        let archived = notesForCategory.filter(elem => {return elem.archived == true })
+        let active = notesForCategory.filter(elem => {return elem.active == true })
+        if(notesForCategory.length == 0) {
+            console
+        } else {
+            createTdNodeForStats(categoryRow, cat)
+            createTdNodeForStats(categoryRow, notesForCategory.length)
+            createTdNodeForStats(categoryRow, active.length)
+            createTdNodeForStats(categoryRow, archived.length)
+        }
+    })
+}
+
+function render(noteID, notes) {
+    let notes_area = document.getElementById('notes_area')
+    let stats_area = document.getElementById('stats_area')
+
+    notes.forEach(note => createNoteDOM(noteID, note, notes_area))
+
+    createStatDOM(categories, notes, stats_area)
+}
+
+export default render
--- task1/script.js
+++ task1/script.js
@@ -1,1 +1,58 @@
-import createNoteDOM from './render.js'
+import {createNoteDOM} from './render.js'
+
+let notes = [
+    {
+        id: 10,
+        name : 'Visit the mortician’s ghost',
+        date: '09/01/2022',
+        category: 'note',
+        content: '12/01/2022 visit the mortician’s ghost to demand a purse of legitimate gold, because the stake in my coffin was never slaughtered.',
+        dates: '12/01/2022',
+        archived: false,
+        active: true
+    },
+    {
+        id: 11,
+        name : 'About drinks',
+        date: '09/01/2022',
+        category: 'religion',
+        content: 'have a drink with the local pop',
+        dates: '',
+        archived: false,
+        active: true
+    },
+    {
+        id: 12,
+        name : 'Dance with fox',
+        date: '09/01/2022',
+        category: 'animals',
+        content: 'On Wednesday 4 April 04/04/2022, dance with the bald fox and return her cigarette case',
+        dates: '04/04/2022',
+        archived: false,
+        active: true
+    },
+    {
+        id: 13,
+        name : 'submarine "Alzheimer’s"',
+        date: '09/01/2022',
+        category: 'news',
+        content: '12/12/2021, the submarine "Alzheimer’s" collided over Lenin’s monument with the submarine "Oppenheimer". The case gained a resonance',
+        dates: '12/12/2021',
+        archived: false,
+        active: true
+    },
+    {
+        id: 14,
+        name : 'dwarf',
+        date: '09/01/2022',
+        category: 'note',
+        content: '02/01/2022, dug a spoon with two ants into the hole of an old dwarf.',
+        dates: '02/01/2022',
+        archived: false,
+        active: true
+    }    
+]
+
+window.onload = function() {
+    notes.forEach(note => createNoteDOM(note.id, note))
+}
